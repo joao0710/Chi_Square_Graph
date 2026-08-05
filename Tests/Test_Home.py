a1d62fbc8d28586cc725e3eaf53688a32cdcb114
joao0710 <joao0710@gmail.com>
2026-08-04 21:50:25 -0300

    fisrt test app

--- Tests/Test_Home.py
+++ Tests/Test_Home.py
@@ -1,17 +1,13 @@
 from streamlit.testing.v1 import AppTest
 from pathlib import Path
 
+PATH_APP = Path(__file__).parent.parent / "Home" / "app.py"
 
 def test_app_inicial_streamlit():
     """Testing if streamlit app is working properly"""
-    # 1. Get atual directory
-    directory_atual = Path(__file__).parent
 
-    # 2. Return to root and enter in Home directory
-    path_app = directory_atual.parent / "Home" / "app.py"
-
-    # 3. Take the path app in string
-    at = AppTest.from_file(str(path_app))
+    # -- Take the path app in string
+    at = AppTest.from_file(str(PATH_APP))
     at.run()
 
     assert not at.exception

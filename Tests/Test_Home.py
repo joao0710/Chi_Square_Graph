ba6f2c26af176d233809a1c77553b542b6f901f6
joao0710 <joao0710@gmail.com>
2026-08-04 21:46:26 -0300

    fisrt test app

--- Tests/Test_Home.py
+++ Tests/Test_Home.py
@@ -1,8 +1,17 @@
-import unittest
+from streamlit.testing.v1 import AppTest
+from pathlib import Path
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
 
-if __name__ == '__main__':
-    unittest.main()
+def test_app_inicial_streamlit():
+    """Testing if streamlit app is working properly"""
+    # 1. Get atual directory
+    directory_atual = Path(__file__).parent
+
+    # 2. Return to root and enter in Home directory
+    path_app = directory_atual.parent / "Home" / "app.py"
+
+    # 3. Take the path app in string
+    at = AppTest.from_file(str(path_app))
+    at.run()
+
+    assert not at.exception

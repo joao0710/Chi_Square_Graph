433d4b461ebc3dcb873c9c4a5ff61deac4352540
joao0710 <joao0710@gmail.com>
2026-08-04 21:58:38 -0300

    Fisrt test app

--- Tests/Test_Home.py
+++ Tests/Test_Home.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 PATH_APP = Path(__file__).parent.parent / "Home" / "app.py"
 
-def test_app_inicial_streamlit():
+def test_app_initial_streamlit():
     """Testing if streamlit app is working properly"""
 
     # -- Take the path app in string
@@ -11,3 +11,10 @@ def test_app_inicial_streamlit():
     at.run()
 
     assert not at.exception
+
+def test_initial_elements():
+    """Testing if initial elements are working properly"""
+    at = AppTest.from_file(str(PATH_APP))
+    at.run()
+
+    assert at.title[0].value == "Chi Square Graph"

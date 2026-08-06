9d03e62968b0a20d9d6bd81402cc45e1eadba23e
joao0710 <joao0710@gmail.com>
2026-08-06 19:53:48 -0300

    Adjunting Degrees of Freedom Slider

--- Home/app.py
+++ Home/app.py
@@ -3,11 +3,15 @@ import streamlit as st
 # -- Principal Title Page --
 st.title("Chi Square Graph")
 
-degrees_of_freedom = st.slider(
-    "Degrees of Freedom",
-    min_value=0,
-    max_value=1,
-    value=0
-)
+colunas = st.columns(2)
+
+with colunas[0]:
+    degrees_of_freedom = st.slider(
+        "Degrees of Freedom",
+        min_value=0,
+        max_value=30,
+        value=0
+    )
+    st.write(f"Degrees_of_freedom: {degrees_of_freedom}")
 
 
